8c0408940ef8d4dad28f3320291dd508891bfebe
PFMello <philipmikh@gmail.com>
2023-10-14 21:26:40 -0300

    finalizado sistema teste upload imagens

--- public/scripts/file-preview.js
+++ public/scripts/file-preview.js
@@ -4,8 +4,12 @@ const imagePreviewElement = document.querySelector("#image-preview");
 filePickerElement.addEventListener("change", () => {
   const files = filePickerElement.files;
 
-  if (!files || files.length === 0) return;
+  if (!files || files.length === 0) {
+    imagePreviewElement.style.display = "none";
+    return;
+  }
 
   const picketFile = files[0];
   imagePreviewElement.src = URL.createObjectURL(picketFile);
+  imagePreviewElement.style.display = "block";
 });
